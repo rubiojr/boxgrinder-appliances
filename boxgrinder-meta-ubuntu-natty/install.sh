cf24350189ed1df0afbeb73561f7512829a07874
Sergio Rubio <rubiojr@frameos.org>
2011-11-14 20:25:53 +0100

    * added boxgrinder-meta-ubuntu-natty appliance

---
+++ boxgrinder-meta-ubuntu-natty/install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "Downloading libguestfs packages"
+wget --quiet -r -l1 -H -t1 -nd -N -np -A.deb http://libguestfs.org/download/binaries/ubuntu1104-packages/ > /dev/null
+rm *ocaml*deb libguestfs-doc* libguestfs0-dbg* >/dev/null 2>&1
+echo "Installing guestfs..."
+sudo dpkg -i guestmount* libguestfs* python-guestfs* febootstrap* > /dev/null
+wget --quiet http://rbel.frameos.org/tmp/guestfs-1.14.2.gem 
+sudo gem install --no-ri --no-rdoc guestfs-1.14.2.gem > /dev/null
+sudo gem install --no-ri --no-rdoc boxgrinder-build boxgrinder-ubuntu-plugin > /dev/null
+

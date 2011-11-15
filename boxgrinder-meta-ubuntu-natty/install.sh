5160978b82f9017c7e4bcaada2cdc960c8cc0ca3
Sergio Rubio <rubiojr@frameos.org>
2011-11-15 15:45:43 +0100

    * libguestfs can't be installed with vmbuilder unfortunately

--- boxgrinder-meta-ubuntu-natty/install.sh
+++ boxgrinder-meta-ubuntu-natty/install.sh
@@ -3,9 +3,9 @@ echo "Downloading libguestfs packages"
 wget --quiet -r -l1 -H -t1 -nd -N -np -A.deb http://libguestfs.org/download/binaries/ubuntu1104-packages/ > /dev/null
 rm *ocaml*deb libguestfs-doc* libguestfs0-dbg* >/dev/null 2>&1
 echo "Installing guestfs..."
-apt-get update 
+# Affected by this http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=645119
 dpkg -i *.deb 
-apt-get -fy install 
+apt-get -fy install
 rm -f *.deb 
 wget --quiet http://rbel.frameos.org/tmp/guestfs-1.14.2.gem 
 gem install --no-ri --no-rdoc guestfs-1.14.2.gem 

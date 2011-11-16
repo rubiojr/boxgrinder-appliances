af2602453c19fdefcd4e27779bfc2ace69d87976
Sergio Rubio <rubiojr@frameos.org>
2011-11-16 17:08:27 +0100

    * reorganization

--- abiquo-chef-template-ubuntu-oneiric/post.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-gem install abiquo-chef-agent
-apt-get remove -fy --purge build-essential gcc make libxml2-dev libxslt-dev ruby-dev
-apt-get autoremove --purge -y
-apt-get clean
-sh /root/clean.sh

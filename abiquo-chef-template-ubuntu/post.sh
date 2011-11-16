5944b479a83a7efa22adf6e890f4b6b7f955c90d
Sergio Rubio <rubiojr@frameos.org>
2011-11-16 18:42:08 +0100

    * updated abiquo chef appliances

--- abiquo-chef-template-ubuntu/post.sh
+++ abiquo-chef-template-ubuntu/post.sh
@@ -3,5 +3,6 @@ gem install abiquo-chef-agent
 apt-get remove -fy --purge build-essential gcc make libxml2-dev libxslt-dev ruby-dev
 apt-get autoremove --purge -y
 apt-get clean
+mkdir /var/lib/dhcp3
 mv /etc/init/abiquo-chef-agent.conf.* /etc/init/abiquo-chef-agent.conf
 sh /root/clean.sh

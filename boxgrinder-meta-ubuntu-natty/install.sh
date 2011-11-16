63166381f49a7113038649fc55f99c9e6bdff849
Sergio Rubio <rubiojr@frameos.org>
2011-11-16 11:43:10 +0100

    * fixes in install.sh script

--- boxgrinder-meta-ubuntu-natty/install.sh
+++ boxgrinder-meta-ubuntu-natty/install.sh
@@ -3,7 +3,9 @@ echo "Downloading libguestfs packages"
 wget --quiet -r -l1 -H -t1 -nd -N -np -A.deb http://rbel.frameos.org/misc/libguestfs-ubuntu/natty/ > /dev/null
 echo "Installing guestfs..."
 # Affected by this http://bugs.debian.org/cgi-bin/bugreport.cgi?bug=645119
-DEBIAN_FRONTEND=noninteractive DEBIAN_PRIORITY=critical dpkg -i *.deb
+export DEBIAN_FRONTEND=noninteractive 
+export DEBIAN_PRIORITY=critical 
+dpkg -i *.deb
 apt-get -fy install
 rm -f *.deb 
 wget --quiet http://rbel.frameos.org/tmp/guestfs-1.14.2.gem 
